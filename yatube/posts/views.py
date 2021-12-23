63efb925f762fe5608fe81d1f8c05de2e4172595
icristev <icristev@mail.ru>
2021-12-23 19:51:56 +0300

    4

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -81,11 +81,11 @@ def post_create(request):
 
 
 @login_required
-def post_edit(request, username, post_id):
+def post_edit(request, user, post_id):
     post = get_object_or_404(Post, id=post_id)
     form = PostForm(request.POST or None, instance=post)
     if post.author != request.user:
-        return redirect('post', username=username, post_id=post_id)
+        return redirect('post', user=user, post_id=post_id)
     if form.is_valid():
         form.save()
         # return redirect('posts:post_detail', post_id)
